822e0a3f58a6c6606426ac72d43d746eeeeb4732
Arshh82 <arshilbaig82@gmail.com>
2023-05-21 02:15:21 +0530

    21-05-

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Kidsglass from './Components/kidsglass/Kidsglass';
 import Contactlenses from './Components/contact lenses/Contactlenses';
 import Sunglass from './Components/sun glass/Sunglass';
 import Storelocator from './Components/store locator/Storelocator';
-import HomeC from './Components/Home/HomeC';
 import Test from './Components/Test';
 import CaroselProducta from './Components/Home/CaroselProducta';
 function App() {
@@ -16,7 +15,7 @@ function App() {
     <>
     <Navbarr/>
     <Routes>
-      <Route path='/' element={<HomeC/>}></Route>
+      <Route path='/' element={<Home/>}></Route>
       <Route path='/computerglass' element={<Computerglass/>}></Route>
       <Route path='/kidsglass' element={<Kidsglass/>}></Route>
       <Route path='/contactlenses' element={<Contactlenses/>}></Route>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -10,78 +10,88 @@ const Home = () => {
   let fi4 = new URL("/public/images/HomeCarosel/cimg (4).jpg", import.meta.url)
   let fi5 = new URL("/public/images/Sdiv/image-.JPG", import.meta.url)
   let fi6 = new URL("/public/images/top glass/eyeglass.png", import.meta.url)
+  let fi7 = new URL("/public/images/top glass/sunglass.png", import.meta.url)
+  let fi8 = new URL("/public/images/top glass/computer glass.PNG", import.meta.url)
+  let fi9 = new URL("/public/images/top glass/contact lens.png", import.meta.url)
+  let fi10 = new URL("/public/images/top glass/powerglass.PNG", import.meta.url)
+  let fi11 = new URL("/public/images/top glass/power sunglass.PNG", import.meta.url)
   return (
     <>
       <div className='homecontainer'>
-      <div className='firstdiv'>
-      <div className='imgdiv'>
-        <div><img src={fi6} alt='no-img' className='imgeyecontrol'/></div>
-        <div><center className='headname'><h4>Eyeglass</h4></center></div>
-      </div>
-      <div className='imgdiv'>
-        <div><img src={fi6} alt='no-img' className='imgeyecontrol'/></div>
-        <div><center className='headname'><h4>Eyeglass</h4></center></div>
-      </div>
-      <div className='imgdiv'>
-      <div><img src={fi6} alt='no-img' className='imgeyecontrol'/></div>
-        <div><center className='headname'><h4>Eyeglass</h4></center></div>
-      </div>
-      <div className='imgdiv'>
-      <div><img src={fi6} alt='no-img' className='imgeyecontrol'/></div>
-        <div><center className='headname'><h4>Eyeglass</h4></center></div>
-      </div>
-      <div className='imgdiv'>
-      <div><img src={fi6} alt='no-img' className='imgeyecontrol'/></div>
-        <div><center className='headname'><h4>Eyeglass</h4></center></div>
-      </div>
-      <div className='imgdiv'>
-      <div><img src={fi6} alt='no-img' className='imgeyecontrol'/></div>
-        <div><center className='headname'><h4>Eyeglass</h4></center></div>
-      </div>
-</div>
+        <div className='firstdiv'>
+          <div className='imgdiv'>
+            <div className='imageparent'><img src={fi6} alt='no-img' className='imgeyecontrol' /></div>
+            <div><center className='headname'><p>Eyeglasses</p></center></div>
+          </div>
+
+          <div className='imgdiv'>
+            <div className='imageparent'><img src={fi8} alt='no-img' className='imgeyecontrol' /></div>
+            <div><center className='headname'><p>Computer glasses</p></center></div>
+          </div>
+
+          <div className='imgdiv'>
+            <div className='imageparent'><img src={fi9} alt='no-img' className='imgeyecontrol' /></div>
+            <div><center className='headname'><p>Contact lenses</p></center></div>
+          </div>
+
+          <div className='imgdiv'>
+            <div className='imageparent'><img src={fi10} alt='no-img' className='imgeyecontrol' /></div>
+            <div><center className='headname'><p>Power Sunglasses</p></center></div>
+          </div>
+
+          <div className='imgdiv'>
+            <div className='imageparent'><img src={fi11} alt='no-img' className='imgeyecontrol' /></div>
+            <div><center className='headname'><p>Progressive Lenses</p></center></div>
+          </div>
+
+          <div className='imgdiv'>
+            <div className='imageparent'><img src={fi7} alt='no-img' className='imgeyecontrol' /></div>
+            <div><center className='headname'><p>Sunglasses</p></center></div>
+          </div>
+        </div>
        <div className='slidera'>
-        <Carousel>
-          <Carousel.Item>
-            <img
-              className="d-flex w-100"
-              src={fi1}
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h3>New Upcoming  Summer Sale </h3>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-flex w-100"
-              src={fi2}
-              alt="Second slide"
-            />
-            <Carousel.Caption>
-              <h3>New Upcoming  Summer Sale </h3>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-flex w-100"
-              src={fi3}
-              alt="Third slide"
-            />
-            <Carousel.Caption>
-              <h3>New Upcoming  Summer Sale </h3>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-flex w-100"
-              src={fi4}
-              alt="Third slide"
-            />
-            <Carousel.Caption>
-              <h3>New Upcoming  Summer Sale </h3>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
+          <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-flex w-100"
+                src={fi1}
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>New Upcoming  Summer Sale </h3>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-flex w-100"
+                src={fi2}
+                alt="Second slide"
+              />
+              <Carousel.Caption>
+                <h3>New Upcoming  Summer Sale </h3>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-flex w-100"
+                src={fi3}
+                alt="Third slide"
+              />
+              <Carousel.Caption>
+                <h3>New Upcoming  Summer Sale </h3>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-flex w-100"
+                src={fi4}
+                alt="Third slide"
+              />
+              <Carousel.Caption>
+                <h3>New Upcoming  Summer Sale </h3>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
         </div>
         <div className='seconddiv'>
           <h3 className='stext'>Do More Be more</h3>
@@ -94,7 +104,7 @@ const Home = () => {
             <span className='trend'>The Trend</span>
           </div>
           <div className='ctdivb'>
-            <CaroselProducta/>
+            <CaroselProducta />
           </div>
         </div>
       </div>
