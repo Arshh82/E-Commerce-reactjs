fa1ad60cf16f54d65555f63fac94663c2a60a21f
Arshh82 <arshilbaig82@gmail.com>
2023-06-05 22:48:31 +0530

    second

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { AppProvider } from './Context/ProductContext';
+import Context from './Context/Context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <Context>
     <App />
+    </Context>
 );
 
 // If you want to start measuring performance in your app, pass a function
