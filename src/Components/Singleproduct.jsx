96cbc4c90fea030eff3e8f2c7c848189992636e0
Arshh82 <arshilbaig82@gmail.com>
2023-06-05 22:48:22 +0530

    first

--- src/Components/Singleproduct.jsx
+++ src/Components/Singleproduct.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import axios from 'axios';
 import './Singleproduct.css'
 import Loading from '../Loading';
@@ -32,17 +32,7 @@ function Singleproduct({productt}) {
     setProduct(res.data);
     setLoading(true);
   }
-
   const [amount,setAmount] = useState(1);
-  
-  // const setDcrease = () => {
-  //   amount > 1 ? setAmount(amount -1) : setAmount(1);
-  // };
-
-  // const setIncrease = () => {
-  //   amount<stockqwantity ? setAmount(amount+1) : setAmount(stockqwantity);
-  // };
-  
   return (
     <>
       <div>
@@ -60,13 +50,16 @@ function Singleproduct({productt}) {
                     <p className='detailsize mb-2'>Size: {v.size}</p>
                     <p className='detailamount mb-2'>Rs. {v.amount}/-</p>
                     <h3 className='mb-2'>Availability:{v.stockqwantity >= 1 ? <h4 className='text-success'>In stock</h4> : <h4 className='text-danger'>Out of stock</h4>}</h3>
-                    <h3 className='mb-4'>{v.stockqwantity <= 10 ? <h4 className='text-danger'>Hurry Now Few Stocks Are Left only !</h4> : <h4 className='text-danger'></h4>}</h3>
+                    <h3 className='mb-4'>{v.stockqwantity <= 10 ? <h4 className='text-danger'>Hurry Now Only {v.stockqwantity} Stocks Are Left only !</h4> : <h4 className='text-danger'></h4>}</h3>
                     <button className='detailbtn mb-4'>Buy Now</button>
-                    <button className='detailbtnb' onClick={ () => {alert('Added to CArt')}}>Add to Cart</button>
+                    <Link to='/cart'>
+                    <button className='detailbtnb'  onclick={() => addTocart(id,amount,) }>Add to Cart</button>
+                    </Link>
                     <div className='proqwantity'>
                       <button className='btn btn-secondary' onClick={(setDcrease) => { amount > 1 ? setAmount(amount - 1) : setAmount(1); }}> <FiMinus /></button>
                       <div>{amount}</div>
-                      <button className='btn btn-primary' onClick={(setIncrease) => { amount < v.stockqwantity ? setAmount(amount + 1) : setAmount(v.stockqwantity);
+                      <button className='btn btn-primary' onClick={(setIncrease) => {
+                        amount < v.stockqwantity ? setAmount(amount + 1) : setAmount(v.stockqwantity);
                       }}><FiPlus /></button>
                     </div>
                   </div>
