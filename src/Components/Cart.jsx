84b8dcc80879ba57949cb54f77f59999d22a1bc8
Arshh82 <arshilbaig82@gmail.com>
2023-06-09 23:14:20 +0530

    11-14-

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,31 +1,59 @@
 import React, { useState } from 'react'
-import Singleproduct from './Singleproduct'
+import { getNodeText } from '@testing-library/react';
+import { useSelector, useDispatch } from 'react-redux';
+import { remove } from '../Reducers/cartSlice';
 
-const API = 'https://arshil-eyewear.onrender.com/Eyeglassrangeapi/'
 const Cart = () => {
-  // const [amount,setAmount] = useState(1);
-
-  // const setDcrease = () => {
-  //   amount > 1 ? setAmount(amount -1) : setAmount(1);
-  // };
-
-  // const setIncrease = () => {
-  //   amount<stock ? setAmount(amount+1) : setAmount(stock);
-  // };
-
+  const dispatch = useDispatch();
+    const products = useSelector((state) => state.cart);
+    const handleRemove = (productId) => {
+        dispatch(remove(productId));
+    };
+    console.log(products)
+    
+const [amount,setAmount]= useState(1)
   return (
-    <div>
-    <p>Cart</p>
-    <p>Cart</p>
-    <p>Cart</p>
-    <p>Cart</p>
-    <p>Cart</p>
-    <p>Cart</p>
-    <p>Cart</p>
-    <p>Cart</p>
-    
-    
-    </div>
+    <>
+
+      <div className='d-flex w-100 h-30'>
+      </div>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <h3>Cart</h3>
+            <div className="cartWrapper">
+                {products.map((product) => (
+                    <div key={product.id} className="cartCard">
+                    <table className='table table-bordered text-center table-hover '>
+                    <tbody>
+                    <td>
+                        <img src={product.image} alt="--" className='d-flex w-20 h-30' /></td>
+                        <td><h5>{product.name}</h5></td>
+                        <td><h5>{product.amount}</h5></td>
+                        <td><h5>{product.stockqwantity}</h5></td>
+                       <td> <button
+                            className="btn"
+                            onClick={() => handleRemove(product.id)}
+                        >
+                            Remove
+                        </button></td>
+                        <td>
+                          <div className='proqwantity'>
+                      <button className='btn btn-secondary' onClick={(setDcrease) => { amount > 1 ? setAmount(amount - 1) : setAmount(1); }}> -</button>
+                      <div>{amount}</div>
+                      <button className='btn btn-primary' onClick={(setIncrease) => {
+                        amount < product.stockqwantity ? setAmount(amount + 1) : setAmount(product.stockqwantity);
+                      }}>+</button>
+                    </div>
+                        </td>
+
+                        </tbody>
+                        </table>
+                    </div>
+                ))}
+            </div>
+
+    </>
   )
 }
 
--- src/Components/Singleproduct.jsx
+++ src/Components/Singleproduct.jsx
@@ -5,38 +5,45 @@ import './Singleproduct.css'
 import Loading from '../Loading';
 import { FiPlus,FiMinus } from "react-icons/fi";
 import Cart from './Cart';
+import { useDispatch, useSelector } from 'react-redux';
+import { add } from '../Reducers/cartSlice';
 
 
 
 const API = "https://arshil-eyewear.onrender.com/Eyeglassrangeapi/"
-function Singleproduct({productt}) {
+function Singleproduct() {
+  const dispatch = useDispatch();
+  const { data: produc, status } = useSelector((state) => state.product);
   
-  const [loading,setLoading] = useState(false);
-  const [product, setProduct] = useState([
-    {
-      "id": "",
-      "name": "",
-      "image": "",
-      "imagehov": "",
-      "size": "",
-      "amount": "",
-    }
-  ]);
+  const [loading,setLoading] = useState(true);
+  // const [product, setProduct] = useState([
+  //   {
+  //     "id": "",
+  //     "name": "",
+  //     "image": "",
+  //     "imagehov": "",
+  //     "size": "",
+  //     "amount": "",
+  //   }
+  // ]);
   const { id } = useParams();
   // console.log(id)
-  useEffect(() => {
-    show();
-  });
-  async function show() {
-    var res = await axios.get(API);
-    setProduct(res.data);
-    setLoading(true);
-  }
-  const [amount,setAmount] = useState(1);
+  // useEffect(() => {
+  //   show();
+  // });
+  // async function show() {
+  //   var res = await axios.get(API);
+  //   setProduct(res.data);
+  //   setLoading(true);
+  // }
+
+  const handleAdd = (product) => {
+    dispatch(add(product));
+};
   return (
     <>
       <div>
-        {loading ? product.map((v) => {
+        { products.map((v) => {
           return (
             <div key={v.id} >
               {v.id === id ?
@@ -52,24 +59,15 @@ function Singleproduct({productt}) {
                     <h3 className='mb-2'>Availability:{v.stockqwantity >= 1 ? <h4 className='text-success'>In stock</h4> : <h4 className='text-danger'>Out of stock</h4>}</h3>
                     <h3 className='mb-4'>{v.stockqwantity <= 10 ? <h4 className='text-danger'>Hurry Now Only {v.stockqwantity} Stocks Are Left only !</h4> : <h4 className='text-danger'></h4>}</h3>
                     <button className='detailbtn mb-4'>Buy Now</button>
-                    <Link to='/cart'>
-                    <button className='detailbtnb'  onclick={() => addTocart(id,amount,) }>Add to Cart</button>
-                    </Link>
-                    <div className='proqwantity'>
-                      <button className='btn btn-secondary' onClick={(setDcrease) => { amount > 1 ? setAmount(amount - 1) : setAmount(1); }}> <FiMinus /></button>
-                      <div>{amount}</div>
-                      <button className='btn btn-primary' onClick={(setIncrease) => {
-                        amount < v.stockqwantity ? setAmount(amount + 1) : setAmount(v.stockqwantity);
-                      }}><FiPlus /></button>
-                    </div>
+                    
+                    <button className='detailbtnb' onClick={() => handleAdd(v)} >Add to Cart</button>
+                    
                   </div>
                 </div>
                 : <p></p>
               }
             </div>)
-        }) : <div className='loadingg'>
-          <Loading />
-        </div>
+        }) 
         }
       </div>
     </>
